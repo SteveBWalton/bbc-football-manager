f42380a1c7df924c9cbc501e8f94bb088594a87e
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2018-07-13 10:03:19 +0100

    Added a CGame object.

---
+++ modGame.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Module to implement the CGame class for the the BBC Football Manager program.
+'''
+
+# System libraries.
+
+# Application Libraries.
+
+
+class CGame:
+    ''' Class to represent the BBC Football Manager game. '''
+
+
+
+    def __init__(self):
+        ''' Class constructor for the BBC Football manager game. '''
+        self.player_name = ''
+
+
+
+    def Football(self):
+        ''' Implementation of DEFPROCfootball().  Display a title. '''
+        print('┏━━             ┃       ┃ ┃   ┏━┳━┓')
+        print('┃            ┃  ┃       ┃ ┃   ┃ ┃ ┃' )
+        print('┣━━ ┏━┓ ┏━┓ ━#━ ┣━┓ ━━┓ ┃ ┃   ┃   ┃ ━━┓ ━┳━┓ ━━┓ ┏━┓ ┏━┓ ┏━')
+        print('┃   ┃ ┃ ┃ ┃  ┃  ┃ ┃ ┏━┫ ┃ ┃   ┃   ┃ ┏━┃  ┃ ┃ ┏━┫ ┃ ┃ ┣━┛ ┃')
+        print('┃   ┗━┛ ┗━┛  ┃  ┗#┛ ┗━┛ ┃ ┃   ┃   ┃ ┗━┛  ┃ ┃ ┗━┛ ┗━┫ ┗━━ ┃')
+        print('                                                   ┃')
+        print('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛')
+        print('By Steve Walton BBC BASIC 1982-1989, 2000, Python 2018.')
+
+
